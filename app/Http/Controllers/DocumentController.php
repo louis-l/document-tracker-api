8e87661469047c28a4ae4c869bbc01fda13bd19c
Louis L <louis.luu92@gmail.com>
2025-05-01 07:55:53 +1000

    Handle PDF upload

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\DocumentResource;
 use App\Models\Document;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
 
 class DocumentController extends Controller
 {
@@ -23,12 +24,21 @@ class DocumentController extends Controller
 
     public function store(Request $request)
     {
-        $document = $request
-            ->user()
-            ->documents()
-            ->create(
-                $request->all()
-            );
+        // TODO: Should create a proper Request class for this method.
+        $request->validate([
+            'document' => ['required', File::types('pdf')->extensions('pdf')->max(20 * 1024)],
+            'expires_at' => ['nullable', 'date', 'after:today'],
+        ]);
+
+        $user = $request->user();
+        $file = $request->file('document');
+
+        $document = new Document();
+        $document->name = $file->getClientOriginalName();
+        $document->path = $file->store('documents/'.$user->id);
+        $document->owner_id = $user->id;
+        $document->expires_at = $request->date('expires_at');
+        $document->save();
 
         return DocumentResource::make($document);
     }
